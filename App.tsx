5c52d50c8398ce3236f413972d154a42fae171a4
SteadyJingo <jacob.w.corum@gmail.com>
2022-02-20 13:14:18 -0700

    Sentry and eslint set up

--- App.tsx
+++ App.tsx
@@ -1,5 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Sentry from 'sentry-expo';
+
+Sentry.init({
+  dsn: 'https://b10102610f4140648a82884d46a13d89@o1140961.ingest.sentry.io/6220378',
+  enableInExpoDevelopment: false,
+  debug: true, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
+});
 
 export default function App() {
   return (
