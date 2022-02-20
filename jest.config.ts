13985505e1b377c2d4de9c209b32ec04c87cf111
SteadyJingo <jacob.w.corum@gmail.com>
2022-02-20 15:53:27 -0700

    updated jest config to be preset to jest-expo and imported react-rest-renderer

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,7 @@ import type {Config} from '@jest/types'
 export default async (): Promise<Config.InitialOptions> => {
   return {
     verbose: true,
-    preset: "react-native",
+    preset: "jest-expo",
     setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
     transformIgnorePatterns: [
       "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)"
