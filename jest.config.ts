2060fe362d091c6e128ebcbe8cd1424e0ee1d397
SteadyJingo <jacob.w.corum@gmail.com>
2022-02-20 13:47:39 -0700

    added modulePaths to jest.config.ts to pass ci

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,9 @@ export default async (): Promise<Config.InitialOptions> => {
   return {
     verbose: true,
     preset: "jest-expo",
+    modulePaths: [
+      "<rootDir>"
+    ],
     transformIgnorePatterns: [
       "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)"
     ],
